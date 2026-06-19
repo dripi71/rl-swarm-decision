fix: 1) fixed vote reward hacking + better gradient 2) quality_score is prior-free

agents/agent.py
@@ -1,5 +1,6 @@
-import numpy as np 
+import numpy as np
 import random
+
 
 class Agent:
     def __init__(self, id, config):
@@ -13,23 +14,4 @@ class Agent:
         self.events_at_location = [0 for _ in range(self.num_locations)]
         self.steps_at_current_location = 0
         self.uncertainties_before = np.ones(self.num_locations, dtype=np.float32)
-
-        # For Bayesian Baseline
-        #self.obs_time = 1000   # initial 10³ timesteps
-        #self.location_visits = [0 for _ in range(self.num_locations)]
-        #self.last_ten_visits_at_loc = [deque(maxlen=10) for _ in range(self.num_locations)]
-        #self.event_counter = 0
-        #self.initial_random_wait = random.randint(1, 500) # Reference : https://github.com/StudentWorkCPS/gamma_bots/blob/master/swarmy/experiment.py
-
-    def update_vote(self):
-        #todo: delete calls of update_vote
-        pass
-    
-    def get_confidence(self):
-        D = []
-        for i in range(self.num_locations):
-            d = self.events_at_location[i] / (self.timesteps_at_location[i] + 1)
-            D.append(d)
-        return D
-           
-
+        self.uncertainty_at_last_vote = np.ones(self.num_locations, dtype=np.float32)

swarm_environment/env/swarm_environment.py
@@ -173,21 +173,26 @@ class SwarmDecisionEnvironment(AECEnv):
             events = np.array(agent.events_at_location, dtype=np.float32)
             timesteps = np.array(agent.timesteps_at_location, dtype=np.float32)
             posterior_rate = (events + alpha_0) / (timesteps + beta_0)
-            prior_rate = (0 + alpha_0) / (1 + beta_0)
-            quality_score = 1.0 - (posterior_rate / prior_rate)
+
+            max_posterior = np.max(posterior_rate)
+            quality_score = 1.0 - (posterior_rate / max_posterior)
             best_by_quality = np.max(quality_score)
             is_best_choice = np.isclose(
                 quality_score[vote_action], best_by_quality, atol=1e-5
             )
             uncertainty_of_vote = 1.0 / np.sqrt(events[vote_action] + alpha_0)
-            if is_best_choice and uncertainty_of_vote < 0.2:
-                r_vote = 1.0
-            elif not is_best_choice:
-                r_vote = -1.0
-            else:
-                r_vote = 0.0
+            prev_uncertainty = agent.uncertainty_at_last_vote[vote_action]
+            uncertainty_reduction = prev_uncertainty - uncertainty_of_vote
 
-            self.rewards[agent.id] += r_vote * self.config["rewards"]["r_vote_amp"]
+            if is_best_choice:
+                r_vote = (
+                    max(0.0, uncertainty_reduction)
+                    * self.config["rewards"]["r_vote_amp"]
+                )
+            else:
+                r_vote = -1.0
+
+            self.rewards[agent.id] += r_vote
 
             self.episode_stats["votes_cast"] += 1
 
@@ -269,13 +274,14 @@ class SwarmDecisionEnvironment(AECEnv):
 
         posterior_rate = (events + alpha_0) / (timesteps + beta_0)
         relative_uncertainty = 1.0 / np.sqrt(events + alpha_0)
-        prior_rate = (0 + alpha_0) / (1 + beta_0)
-        quality_score = 1.0 - (posterior_rate / prior_rate)
+
+        max_posterior = np.max(posterior_rate)
+        quality_score = 1.0 - (posterior_rate / max_posterior)
 
         # Two very important observation metrics: relative uncertainty and quality score
         # The relative uncertainty shows how close the agent is to the true rate, only improves if events are spottet!
         # Absolute uncertainty is useless here because it would be certain that the lambda is low (But we know that already! (prior))
-        # Quality score: how good the location is compared to the other locations
+        # Quality score: how good the location is compared to the other locations, inverted because high quality -> good location, low events
 
         self_vote = np.zeros(num_locations, dtype=np.float32)
         if agent.current_vote is not None: